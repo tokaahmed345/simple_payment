72cbdaab6a5798ce23a5b026d0475001a7623297
tokaahmed354 <tokaahmed34567@gmail.com>
2025-06-26 14:36:16 +0200

    initialcommit

--- lib/core/utils/colors/colors.dart
+++ lib/core/utils/colors/colors.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const white=Colors.white;
-  static const black=Colors.black;
-
+  static const black=Color(0xff363130);
+  static const salmon=Color(0xfff4b5a4);
+  static const teracotta=Color(0xffcc7861);
+ static const brownRosy= Color(0xffdcbeb6);
+static const beige=Color(0xfffaf0e6);
+static const orangeDark=Color(0xff4b4544);
 }
