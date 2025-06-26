a25b67efd20929c6682f2266f3deb4e8958fd608
tokaahmed354 <tokaahmed34567@gmail.com>
2025-06-26 23:34:10 +0200

    refactor radio button

--- lib/feature/payment/presentation/views/widgets/payment_methods_view_body.dart
+++ lib/feature/payment/presentation/views/widgets/payment_methods_view_body.dart
@@ -6,8 +6,16 @@ import 'package:simplepaymentcard/core/utils/styles/style.dart';
 import 'package:simplepaymentcard/feature/payment/presentation/views/widgets/custom_appbar_section.dart';
 import 'package:simplepaymentcard/feature/payment/presentation/views/widgets/custom_payment_methods_body_section.dart';
 
-class PaymentMethodsViewBody extends StatelessWidget {
+class PaymentMethodsViewBody extends StatefulWidget {
   const PaymentMethodsViewBody({super.key});
+
+  @override
+  State<PaymentMethodsViewBody> createState() => _PaymentMethodsViewBodyState();
+}
+
+class _PaymentMethodsViewBodyState extends State<PaymentMethodsViewBody> {
+  int selectedIndexed=0;
+    int index=0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +27,32 @@ class PaymentMethodsViewBody extends StatelessWidget {
         padding: EdgeInsets.symmetric(vertical: 40.0),
         child: CustomAppBarSection(title: "Payment Method",),
       ),
-     const CustomBodySecion(text: '*** *** *** 67',icon: Icons.credit_card,),
-      const CustomBodySecion(text: 'Apple Play',icon: Icons.apple_outlined),
-     const CustomBodySecion(text: 'PayPal',icon: Icons.paypal_outlined,),
-      const CustomBodySecion(text: 'Cash',icon: Icons.attach_money,),
+      CustomBodySecion
+     (
+      selectedIndex: selectedIndexed,
+      index: 0,
+      onTap: (){setState(() {
+        selectedIndexed=0;
+      });},
+      text: '*** *** *** 67',icon: Icons.credit_card,),
+       CustomBodySecion( selectedIndex: selectedIndexed,
+      index: 1,
+      onTap: (){setState(() {
+        selectedIndexed=1;
+      });},text: 'Apple Play',image: 'assets/images/apple.png',),
+      CustomBodySecion( selectedIndex: selectedIndexed,
+      index: 2,
+      onTap: (){setState(() {
+        selectedIndexed=2;
+      });},text: 'PayPal',image: 'assets/images/apple.png',),
+       CustomBodySecion( selectedIndex: selectedIndexed,
+      index: 3,
+      onTap: (){setState(() {
+        selectedIndexed=3;
+      });},text: 'Cash',icon: Icons.attach_money,),
       const Spacer(),
 
-// CustomButton(text: 'Add New Card',onPressed:(){
-//   Get.toNamed(AppRoutes.paymentCard);
-
-// } 
+//
 TextButton(onPressed: (){Get.toNamed(AppRoutes.paymentCard);},  child:const Text("Add New Card",style:AppStyles.textStyle24,) )
 ,
        const SizedBox(height: 60,)
