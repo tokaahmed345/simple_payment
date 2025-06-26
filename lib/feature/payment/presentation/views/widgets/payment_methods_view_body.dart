de1ab368b60dc0726f86681fe37bd37960967593
Kareem <karemthabet>
2025-06-27 01:16:49 +0300

    merge

--- lib/feature/payment/presentation/views/widgets/payment_methods_view_body.dart
+++ lib/feature/payment/presentation/views/widgets/payment_methods_view_body.dart
@@ -3,7 +3,6 @@ import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:simplepaymentcard/core/utils/router/routes.dart';
 import 'package:simplepaymentcard/core/utils/styles/style.dart';
-import 'package:simplepaymentcard/feature/payment/presentation/views/widgets/custom_appbar_section.dart';
 import 'package:simplepaymentcard/feature/payment/presentation/views/widgets/custom_payment_methods_body_section.dart';
 
 class PaymentMethodsViewBody extends StatefulWidget {
@@ -20,42 +19,38 @@ class _PaymentMethodsViewBodyState extends State<PaymentMethodsViewBody> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  const EdgeInsets.symmetric(horizontal: 10),
-      child:  Column(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
         children: [
-      const Padding(
-        padding: EdgeInsets.symmetric(vertical: 40.0),
-        child: CustomAppBarSection(title: "Payment Method",),
-      ),
-      CustomBodySecion
-     (
-      selectedIndex: selectedIndexed,
-      index: 0,
-      onTap: (){setState(() {
-        selectedIndexed=0;
-      });},
-      text: '*** *** *** 67',icon: Icons.credit_card,),
-       CustomBodySecion( selectedIndex: selectedIndexed,
-      index: 1,
-      onTap: (){setState(() {
-        selectedIndexed=1;
-      });},text: 'Apple Play',image: 'assets/images/apple.png',),
-      CustomBodySecion( selectedIndex: selectedIndexed,
-      index: 2,
-      onTap: (){setState(() {
-        selectedIndexed=2;
-      });},text: 'PayPal',image: 'assets/images/paypal.png',),
-       CustomBodySecion( selectedIndex: selectedIndexed,
-      index: 3,
-      onTap: (){setState(() {
-        selectedIndexed=3;
-      });},text: 'Cash',icon: Icons.attach_money,),
-      const Spacer(),
+          const CustomBodySecion(
+            text: '*** *** *** 67',
+            icon: Icons.credit_card,
+          ),
+          const CustomBodySecion(
+              text: 'Apple Play', icon: Icons.apple_outlined),
+          const CustomBodySecion(
+            text: 'PayPal',
+            icon: Icons.paypal_outlined,
+          ),
+          const CustomBodySecion(
+            text: 'Cash',
+            icon: Icons.attach_money,
+          ),
+          const Spacer(),
 
-//
-TextButton(onPressed: (){Get.toNamed(AppRoutes.paymentCard);},  child:const Text("Add New Card",style:AppStyles.textStyle24,) )
-,
-       const SizedBox(height: 60,)
+// CustomButton(text: 'Add New Card',onPressed:(){
+//   Get.toNamed(AppRoutes.paymentCard);
+
+// }
+          TextButton(
+              onPressed: () {
+                Get.toNamed(AppRoutes.paymentCard);
+              },
+              child: const Text(
+                "Add New Card",
+                style: AppStyles.textStyle24,
+              )),
+         const Spacer(),
         ],
       ),
     );
